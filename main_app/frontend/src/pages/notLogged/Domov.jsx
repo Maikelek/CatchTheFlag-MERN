accb85d52cecaa35dbfd787c4d8098bb2d9016fc
Marián Zelinka <96266137+Maikelek@users.noreply.github.com>
2023-05-18 16:55:50 +0200

    Typo

--- main_app/frontend/src/pages/notLogged/Domov.jsx
+++ main_app/frontend/src/pages/notLogged/Domov.jsx
@@ -17,7 +17,7 @@ const Domov = () => {
             <p>
             This is a website project focused on working with web technologies, social networks, encryption, forensic and malware analysis. Its task is to increase awareness of these technologies and their dangers. The competition is suitable mainly for high school and higher education students.
             <br></br><br></br>
-            It is a /catch the flag\ game. To move on to the next level, it is necessary to find a password in the form <strong>/lx-password</strong> on the website using the technologies mentioned above. Alternatively, it may be specified differently in a specific level.
+            It is a /catch the flag\ game. To move on to the next level, it is necessary to find a password in the form <strong>/lx-password\</strong> on the website using the technologies mentioned above. Alternatively, it may be specified differently in a specific level.
             <br></br><br></br>
             The project is inspired by CTF competitions such as TryhackMe, HackTheBox, CyberGame, HackQuest.
 
